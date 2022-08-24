816eebaa926cc227892bab9c74ec30cafd91de1d
Yuvraj Mann <yuvrajmann1@gmail.com>
2022-08-24 12:56:02 +0530

    refactor

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Language - Python v3.10.6
+# Framework used for developing desktop application - customtkinter v0.3
+
 from calendar import week
 from cgitb import text
 from doctest import master
@@ -8,16 +11,18 @@ import customtkinter
 import pandas as pd
 import processDataFrames
 from tkinter import messagebox
+import os
 
 #---------------------------- Window Configuration ----------------------------#
 
 customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
     
-window=customtkinter.CTk()
+window=customtkinter.CTk() #Initializing root window
 window.title('Hours to charge calculator')
 window.geometry('660x560')
 window.minsize(660, 560)
+
 #---------------------------- Constant Values ----------------------------#
 
 NO_FILE_SELECTED="Not File Selected"
@@ -26,15 +31,15 @@ NO_FOLDER_SELECTED="No Folder Selected"
 #---------------------------- Data ----------------------------#
 
 # Uploaded Files Name
-UploadClappedActionFileName=NO_FILE_SELECTED
-UploadAccessActualActionFileName=NO_FILE_SELECTED
-UploadCustomReportActionFileName=NO_FILE_SELECTED
-selectUploadFolder=NO_FOLDER_SELECTED
+UploadClappedActionFileName=NO_FILE_SELECTED #The clapped hours file location
+UploadAccessActualActionFileName=NO_FILE_SELECTED #The actual access file location
+UploadCustomReportActionFileName=NO_FILE_SELECTED # The umanity custom report file location
+selectUploadFolder=NO_FOLDER_SELECTED #The upload folder location
 
 #uploaded Files Data frames
-clappedHoursDataframe=0
-clientActualAccessDataframe=0
-clientCustomReportDataframe=0
+clappedHoursDataframe=0 #The clapped hours file
+clientActualAccessDataframe=0 #The actual access file
+clientCustomReportDataframe=0 # The umanity custom report file
 numberOfWeeks=0
 
 #---------------------------- Utility Function ----------------------------#
@@ -80,10 +85,16 @@ def UploadCustomReportAction(event=None):
 
     label_4.config(text=UploadCustomReportActionFileName)
 
-# Function selects upload folder
+# Function to select output folder
 def selectUploadFolderUtil(event=None):
-    global selectUploadFolder
-    selectUploadFolder = filedialog.askdirectory()
+    try:
+        global selectUploadFolder
+        selectUploadFolder = filedialog.askdirectory()
+        if selectUploadFolder=="":
+            selectUploadFolder=NO_FOLDER_SELECTED
+    except:
+        selectUploadFolder = NO_FOLDER_SELECTED
+
     label_5.config(text=selectUploadFolder)
 
 # Function selects number of weeks-client clapped
@@ -93,73 +104,93 @@ def clientClappedNumberOfWeeks():
     numberOfWeeks=dialog.get_input()
     label_6.config(text=(f"Number of weeks - {numberOfWeeks}"))
 
+#Utility function used to process all selected input files and calculate cost
 def framesProcess():
-    processDataFrames.processDataFrame(clappedHoursDataframe,clientActualAccessDataframe,clientCustomReportDataframe,numberOfWeeks,selectUploadFolder)
+    processDataFrames.processDataFrame(window,clappedHoursDataframe,clientActualAccessDataframe,clientCustomReportDataframe,numberOfWeeks,selectUploadFolder)
+
 #---------------------------- UI Elements ----------------------------#
-frameLeft = customtkinter.CTkFrame(master=window,
+
+# Whole UI divided into two frames - Upper and lower frame
+
+#Configuring and placing upper frame
+frameUpper = customtkinter.CTkFrame(master=window,
                                width=300,
                                corner_radius=10)
-frameLeft.pack(fill="x",padx=40,pady=40)
+frameUpper.pack(fill="x",padx=40,pady=40)
 
-frameRight = customtkinter.CTkFrame(master=window,
+#Configuring and placing the lower frame
+frameLower = customtkinter.CTkFrame(master=window,
                                width=110,
                                corner_radius=10)
 
-frameRight.pack(fill="x",padx=40)
+frameLower.pack(fill="x",padx=40)
 
-label_1 = customtkinter.CTkLabel(master=frameLeft,
-                                              text="Select Files",
+# Text displaying - select input files
+label_1 = customtkinter.CTkLabel(master=frameUpper,
+                                              text="Select Input Files",
                                               text_font=("Roboto Medium", -16)) 
 
 label_1.pack(fill="x",pady=10,padx=4)
 
-clientClappedbutton = customtkinter.CTkButton(master=frameLeft, text="Client Capped Hours File", command=UploadClappedAction)
-clientClappedbutton.pack(fill="x",pady=5,padx=20)
+# Configuring and placing button for selecting client clapped hours file
+clientClappedbutton = customtkinter.CTkButton(master=frameUpper,fg_color="#1597BB",hover_color="#107793", text="Client Capped Hours File", command=UploadClappedAction)
+clientClappedbutton.pack(pady=5,padx=20)
 
-label_2 = customtkinter.CTkLabel(master=frameLeft,
+# Label displaying the clapped hours file upload status
+label_2 = customtkinter.CTkLabel(master=frameUpper,
                                               text=UploadClappedActionFileName,
                                               text_font=("Roboto Medium", -11)) 
 
-label_2.pack(fill="x",pady=1,padx=4)
+label_2.pack(pady=1,padx=4)
 
-clientAccessActual = customtkinter.CTkButton(master=frameLeft, text="Client Access File", command=UploadAccessActualAction)
-clientAccessActual.pack(fill="x",pady=5,padx=20)
-label_3 = customtkinter.CTkLabel(master=frameLeft,
+# Configuring and placing button for selecting client actual access file
+clientAccessActual = customtkinter.CTkButton(master=frameUpper, fg_color="#1597BB",hover_color="#107793",text="Client Access File", command=UploadAccessActualAction)
+clientAccessActual.pack(pady=5,padx=20)
+
+# Label displaying the client actual access file upload status
+label_3 = customtkinter.CTkLabel(master=frameUpper,
                                               text=UploadAccessActualActionFileName,
                                               text_font=("Roboto Medium", -11)) 
 
-label_3.pack(fill="x",pady=1,padx=4)
+label_3.pack(pady=1,padx=4)
 
-clientCustomReport=customtkinter.CTkButton(master=frameLeft,text="CRIB Sheet",command=UploadCustomReportAction)
-clientCustomReport.pack(fill="x",pady=5,padx=20)
+# Configuring and placing button for selecting umanity custom report file
+clientCustomReport=customtkinter.CTkButton(master=frameUpper,text="Humanity custom report",fg_color="#1597BB",hover_color="#107793",command=UploadCustomReportAction)
+clientCustomReport.pack(pady=5,padx=20)
 
-weekSelect = customtkinter.CTkButton(master=frameLeft, text="Number of weeks - Client Clapped", command=clientClappedNumberOfWeeks)
-weekSelect.pack(fill="x",pady=5,padx=20)
-
-label_4 = customtkinter.CTkLabel(master=frameLeft,
+# Label displaying the umanity custom report file upload status
+label_4 = customtkinter.CTkLabel(master=frameUpper,
                                               text=UploadCustomReportActionFileName,
                                               text_font=("Roboto Medium", -11)) 
 
 label_4.pack(fill="x",pady=1,padx=4)
 
-label_6 = customtkinter.CTkLabel(master=frameLeft,
+# Configuring and placing button for selecting selecting the number of weeks
+weekSelect = customtkinter.CTkButton(master=frameUpper, text="Number of weeks",fg_color="#1597BB",hover_color="#107793", command=clientClappedNumberOfWeeks)
+weekSelect.pack(pady=5,padx=20)
+
+# Label displaying the number of weeks
+label_6 = customtkinter.CTkLabel(master=frameUpper,
                                               text="Number of weeks - 0",
                                               text_font=("Roboto Medium", -11))
 label_6.pack(fill="x",pady=1,padx=4)
 
-selectOutputFolder=customtkinter.CTkButton(master=frameLeft,text="Select Output Folder",command=selectUploadFolderUtil)
-selectOutputFolder.pack(fill="x",pady=5,padx=20)
+# Configuring and placing button for selecting selecting the output folder
+selectOutputFolder=customtkinter.CTkButton(master=frameUpper,text="Select Output Folder",fg_color="#1597BB",hover_color="#107793",command=selectUploadFolderUtil)
+selectOutputFolder.pack(pady=5,padx=20)
 
-label_5 = customtkinter.CTkLabel(master=frameLeft,
+# Label displaying the output folder
+label_5 = customtkinter.CTkLabel(master=frameUpper,
                                               text=selectUploadFolder,
                                               text_font=("Roboto Medium", -11)) 
 
 label_5.pack(fill="x",pady=1,padx=4)
 
-processButton=customtkinter.CTkButton(master=frameRight, text="Process Files", command=framesProcess)
-processButton.pack(fill="x",pady=20,padx=20)
+# Configuring and placing button for processing input
+processButton=customtkinter.CTkButton(master=frameLower,text="Calculate Hours", command=framesProcess)
+processButton.pack(pady=20,padx=20)
 
-
+#---------------- Configuration to root window for making it responsive ---------------------#
 window.grid_rowconfigure(0, weight=1)
 window.grid_rowconfigure(1, weight=1)
 window.grid_rowconfigure(2, weight=1)
--- processDataFrames.py
+++ processDataFrames.py
@@ -1,62 +1,186 @@
+# Pandas library in python is used for easing the process of excel files processing
+# Please get a basic understanding of pandas before working on this file
+     
 from http import client
 from tkinter import messagebox
 import pandas as pd
+import os
+from threading import Thread
 
 #---------------------------- Constant Values ----------------------------#
 NO_FILE_SELECTED="Not File Selected"
 NO_FOLDER_SELECTED="No Folder Selected"
 
 def processDataFrame(
+    root,
     clappedHoursDataframe,
     clientActualAccessDataframe,
     clientCustomReportDataframe,
     numberOfWeeks,
     selectUploadFolder
     ):
-    # Null vaue checking
+
+    ## -- Null vaue checking and popping error boxes corresponding to missing files -- ##
     if not isinstance(clappedHoursDataframe,pd.DataFrame):
-        messagebox.showerror('Processing Error', f'Error: Please provide client clapped hours file')
+        messagebox.showerror('Processing Error', f'Error: Please provide client capped hours file')
         return
     if not isinstance(clientActualAccessDataframe,pd.DataFrame):
         messagebox.showerror('Processing Error', f'Error: Please provide client actual access file')
         return
     if not isinstance(clientCustomReportDataframe,pd.DataFrame):
-        messagebox.showerror('Processing Error', f'Error: Please provide CRIB sheet')
+        messagebox.showerror('Processing Error', f'Error: Please provide humanity custom report file')
         return
     if numberOfWeeks==0:
-        messagebox.showerror('Processing Error', f'Error: Please provide number of weeks-Client clapped')
+        messagebox.showerror('Processing Error', f'Error: Please provide number of weeks')
         return
     if selectUploadFolder==NO_FOLDER_SELECTED:
         messagebox.showerror('Processing Error', f'Error: Please provide output folder directory')
         return
-
+    # ------------------------------------------------------------------------------------#
+    
+    # Getting the columns in output data frame
     outputFrameColumns=["Employee ID","Employee Name","Employee Level","Total Working Hours"]
     for client in clappedHoursDataframe['Client Name']:
         outputFrameColumns.append(client)
     outputFrameColumns.append('Total Hours Charged by Employee')
     outputFrameColumns.append('Hours Worked - Hours Charged')
     outputFrameColumns.append('Ratio')
+    # ----------------------------------------------------------------------------------------#
 
+    # Grouping the data in cleint custom report for each employee by employee id to get total hours of each employee
     gk=clientCustomReportDataframe.groupby('employee')
-
+    
+    # The data/rows in output data frame
     outputFrameData=[]
 
+    # Lopping for each row in client's custom report data frame  
     for idx in clientCustomReportDataframe.index:
+        # Getting the total hours for particular employee using grouped by 'gk' dataframe
         totalHrs=gk.get_group(clientCustomReportDataframe['employee'][idx])['total_time'].agg('sum')
+        
+        # -- Gathering data for output sheet row -- # 
         outputSheetRow=[clientCustomReportDataframe['id'][idx],
             clientCustomReportDataframe['employee'][idx],
-            clientCustomReportDataframe['Level'][idx],
+            clientCustomReportDataframe['Level'][idx].split()[0],
             totalHrs
         ]
-        for client in clappedHoursDataframe['Client Name']:
-            outputSheetRow.append(0)
-        outputSheetRow.append(0)
-        outputSheetRow.append(0)
-        outputSheetRow.append(0)
 
+        # Getting all distinct cleints
+        for client in clappedHoursDataframe['Client Name']:
+            # checking if 'cleint' is accessible by the employee using client actual access file
+            rw=clientActualAccessDataframe[clientActualAccessDataframe['Id']==clientCustomReportDataframe['id'][idx]][client]
+            accessible=rw.to_string(index=False)
+
+            # setting 0 if not accessible
+            if accessible=='N':
+                outputSheetRow.append(0)
+            else:
+                outputSheetRow.append("")
+                 
+        # Dummy values for other 3 columns
+        outputSheetRow.append(0)
+        outputSheetRow.append(0)
+        outputSheetRow.append(0)
+        
+        #------------------------------------------#
+
+        #Appending row to otuput frame data
         outputFrameData.append(outputSheetRow)
 
+    # Generating ouput data frame using earier gathered output frame columns and output frame data
     outputDataFrame=pd.DataFrame(outputFrameData,columns=outputFrameColumns)
-    print(outputDataFrame)
-    outputDataFrame.to_excel(f'{selectUploadFolder}/HoursToCharge.xlsx')
+    # Removing duplicate values in dataframe based on employee id
+    outputDataFrame=outputDataFrame.drop_duplicates(subset=['Employee ID'],keep='first')
+
+    # Dictionary stores for each cleint - the total working hours for each employee who have access to the client
+    clientTotalHours={}
+
+    #Initializing the dictionary 
+    for client in clappedHoursDataframe['Client Name']:
+            clientTotalHours[client]=0
+
+    for idx in outputDataFrame.index:
+        for client in clappedHoursDataframe['Client Name']:
+            #checking if client is accessible for employee if it is we will add that employee's total working hours to dictionary
+            if outputDataFrame[client][idx]!=0:
+                clientTotalHours[client]+=outputDataFrame['Total Working Hours'][idx]
+
+    # Calculate hours = (employee total hours/sum of all employees' total hours)*client capped hours
+    # Caluclating the hours value for each employee and each client 
+    for idx in outputDataFrame.index:
+        for client in clappedHoursDataframe['Client Name']:
+            if outputDataFrame[client][idx]!=0:
+                clappedHours=clappedHoursDataframe[clappedHoursDataframe['Client Name']==client]['Capped Hours Per Week']
+                clappedHours=(float)(clappedHours.to_string(index=False))
+                calc=(outputDataFrame['Total Working Hours'][idx]/clientTotalHours[client])*(clappedHours)
+                outputDataFrame.at[idx,client]=calc
+    
+    # Developing a function to round to a multiple
+    def round_to_multiple(number, multiple):
+        return multiple * round(number / multiple)
+
+    #Dictionary for storing sum of cleint hours
+    clientsSum={}
+    #Dictionary for storing rounded sum of cleint hours
+    roundedClientSum={}
+    # Dictionary for storing difference in sum of cleint hours and rounded sum of cleint hours
+    differenceDict={}
+
+    #Initializing the dictionaries
+    for client in clappedHoursDataframe['Client Name']:
+            clientsSum[client]=0
+            roundedClientSum[client]=0
+            differenceDict[client]=0
+
+    #Calculating rounded off value to 0.25 multiple and storing it in output frame
+    for idx in outputDataFrame.index:
+        for client in clappedHoursDataframe['Client Name']:
+            clientsSum[client]+=outputDataFrame[client][idx]
+            multiple_round=round_to_multiple(outputDataFrame[client][idx],0.25)
+            roundedClientSum[client]+=multiple_round
+            outputDataFrame.at[idx,client]=multiple_round
+
+    # Computation performend - 
+    # Find the sum of hours for each client 
+    # If sum exceeds capped hours -> sort in ascending order of "total hours" of employees and start subtracting 0.25 from each individual. If the loop reaches the end with still some hours left to remove, have another iteration
+    # If sum is less than capped hours -> sort in descending order of "total hours" of employees and start adding 0.25 to each individual. If the loop reaches the end with still some hours left to give, have another iteration
+
+    for client in clappedHoursDataframe['Client Name']:
+        differenceDict[client]=clientsSum[client]-roundedClientSum[client]
+        differenceDict[client]=round(differenceDict[client],2)
+        if differenceDict[client]>0:
+            outputDataFrame=outputDataFrame.sort_values('Total Working Hours',ascending=False)
+            while True:
+                for idx in outputDataFrame.index:
+                    if differenceDict[client]>=0.25 :
+                        outputDataFrame.at[idx,client]=outputDataFrame[client][idx]+0.25
+                        differenceDict[client]=differenceDict[client]-0.25
+                    else:
+                        break
+                        
+                if differenceDict[client]<0.25:
+                    break;
+        else:
+            outputDataFrame=outputDataFrame.sort_values('Total Working Hours')
+            while True:
+                for idx in outputDataFrame.index:
+                    if differenceDict[client]>=-0.25 :
+                        outputDataFrame.at[idx,client]=outputDataFrame[client][idx]-0.25
+                        differenceDict[client]=differenceDict[client]+0.25
+                    else:
+                        break
+
+                if differenceDict[client]>-0.25:
+                    break;
+    
+    # Sorting the output frame using index value
+    outputDataFrame=outputDataFrame.sort_index()
+    # Converting the data frame excel and saving it at provided output location with name - HoursToCharge.xlsx
+    outputDataFrame.to_excel(f'{selectUploadFolder}/HoursToCharge.xlsx',index=False)
+    # Success message dialog box
     messagebox.showinfo(f'Processing Done',f'Successfully processed , you can find output file here - {selectUploadFolder}/HoursToCharge.xlsx')
+    # Opeing the final output file on a new thread
+    # More about threads in os - https://www.geeksforgeeks.org/thread-in-operating-system/
+    exceFileOpenThread = Thread(target = lambda: os.system(f'{selectUploadFolder}/HoursToCharge.xlsx'))
+    exceFileOpenThread.start()
+    
